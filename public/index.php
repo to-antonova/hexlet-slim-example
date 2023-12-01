cd16b368046aa10f80e60420cad70a6372db7acf
Tatiana Antonova <to.antonova@mail.ru>
2023-12-01 11:16:42 +0700

    add new user

--- public/index.php
+++ public/index.php
@@ -7,6 +7,9 @@ use Slim\Factory\AppFactory;
 // Контейнеры в этом курсе не рассматриваются (это тема связанная с самим ООП), но если интересно, то посмотрите DI Container
 use DI\Container;
 
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+$usersListFile = 'users.json';
+
 $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория, в которой будут храниться шаблоны
@@ -15,6 +18,7 @@ $container->set('renderer', function () {
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
+
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
@@ -22,21 +26,41 @@ $app->get('/', function ($request, $response) {
     // return $response->write('Welcome to Slim!');
 });
 
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
+
+$app->get('/users', function ($request, $response, $args) use ($users) {
+    $term = $request->getQueryParam('term');
+    $filteredUsers = array_filter($users, function ($user) use ($term){
+        return str_contains($user, $term);
+    });
+    $params = [
+        'users' => $filteredUsers,
+        'user' => $args['user'],
+        'term' => $term
+    ];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
+
+$app->get('/users/new', function ($request, $response) {
+    $id = uniqid();
+    $params = [
+        'user' => ['id' => $id, 'nickname' => '', 'email' => ''],
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, "users/new.phtml", $params);
 });
 
 $app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
-    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
-    // $this в Slim это контейнер зависимостей
+    $params = ['users' => $args['id'], 'nickname' => 'user-' . $args['id']];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
+$app->post('/users', function ($request, $response) use ($usersListFile) {
+    $user = $request->getParsedBodyParam('user');
+    $user['id'] = uniqid();
+    file_put_contents($usersListFile, json_encode($user) . PHP_EOL,FILE_APPEND);
+    return $response->withRedirect('/users', 302);
 });
+
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
